5fee499caaa6513a4096af8df4e4a11b9a69209c
VIGNESHWARAN S <107299416+Vickey-VJ@users.noreply.github.com>
2024-11-16 16:24:47 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import LabelEncoder
 from scipy import stats
 
 # Load the trained models
-random_forest_model = joblib.load('.\model\random_forest.pkl')
-gradient_boosting_model = joblib.load('.\model\gradient_boosting.pkl')
+random_forest_model = joblib.load(r'.\model\random_forest.pkl')
+gradient_boosting_model = joblib.load(r'.\model\gradient_boosting.pkl')
 decision_tree_model = joblib.load('model\decision_tree.pkl')
 naive_bayes_model = joblib.load('model\naive_bayes.pkl')
 
